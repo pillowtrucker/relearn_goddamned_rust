1ab68c4b65924bddb4281cb8529d03db87b9cb54
pillowtrucker <githubino@janitor.chat>
2023-12-07 11:28:29 +0100

    I HATE linear algebra, arrays, matrices, number-crunching brain-dead numerology dog shit

--- src/3.rs
+++ src/3.rs
@@ -1,4 +1,5 @@
 use std::{
+    cmp::{max, min},
     collections::{HashMap, HashSet},
     env,
     fs::File,
@@ -8,7 +9,7 @@ use std::{
 };
 
 use ndarray::{
-    parallel::prelude::{IntoParallelRefIterator, ParallelIterator},
+    parallel::prelude::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator},
     s, ArcArray2, Array2, Axis, Ix2, Shape, Slice, SliceArg,
 };
 
@@ -46,8 +47,8 @@ struct Booger {
 }
 
 fn a(lines: Lines<BufReader<File>>) -> u32 {
-    let mut dim_y = 80;
-    let mut dim_x = 80;
+    let mut dim_y = 256;
+    let mut dim_x = 256;
     let mut grid = ArcArray2::from_elem((dim_y + 1, dim_x + 1), '.');
     let mut y = 0;
     lines.for_each(|l| {
@@ -62,32 +63,55 @@ fn a(lines: Lines<BufReader<File>>) -> u32 {
             };
             grid.reshape((dim_y + 1, dim_x + 1));
             ll.char_indices().for_each(|(i, c)| {
-                grid[[y, i]] = c;
+                grid[[y - 1, i]] = c;
             });
         }
     });
     println!("{:?}", grid);
     let mut h = HashSet::<Booger>::new();
-    grid.indexed_iter().fold(0, |acc, ((xx, yy), elem)| {
+    grid.indexed_iter().for_each(|((yy, xx), elem)| {
         if elem.is_ascii_digit() {
             let mut x_start = xx;
             let mut x_end = xx;
             let y = yy;
+            //            println!("found digit {} at x: {} y: {}", elem, xx, yy);
             loop {
-                if !(x_start > 0 && grid[[y, x_start]].is_ascii_digit()) {
-                    if x_start > 0 {
-                        x_start += 1;
-                    }
+                if !(grid[[y, x_start]].is_ascii_digit()) {
+                    x_start += 1;
                     break;
                 }
-                x_start -= 1;
+                if (x_start > 0) {
+                    x_start -= 1;
+                } else {
+                    break;
+                }
             }
+
             while (x_end <= dim_x && grid[[y, x_end]].is_ascii_digit()) {
                 x_end += 1;
             }
-            let sl = Slice {
+            if (x_end > dim_x) {
+                x_end -= 1;
+            }
+            /*            let sl = Slice {
                 start: x_start as isize,
                 end: Some(x_end as isize),
+                step: 1,
+            };*/
+            //let mut border_start_x = 0;
+            /*            if (x_start == 1) {
+                border_start_x = 2
+            } else {
+                border_start_x = x_start
+            };*/
+            let sl_border_h = Slice {
+                start: max(x_start as isize - 1, 0),
+                end: Some(min(x_end as isize + 1, dim_x as isize)),
+                step: 1,
+            };
+            let sl_border_v = Slice {
+                start: max(y as isize - 1, 0),
+                end: Some(min(y as isize + 2, dim_y as isize)),
                 step: 1,
             };
 
@@ -106,22 +130,29 @@ fn a(lines: Lines<BufReader<File>>) -> u32 {
                 .slice(s![x_start..x_end])
                 .iter()
                 .collect();
+            let num_border_h = grid.slice_axis(Axis(1), sl_border_h);
+            let num_border = num_border_h.slice_axis(Axis(0), sl_border_v);
+            let bogered = num_border
+                .into_par_iter()
+                .all(|bog| *bog == '.' || bog.is_ascii_digit());
 
             println!(
-                "x_start: {:?},x_end: {:?}, num_s: {}",
-                x_start, x_end, num_s
+                "x_start: {:?},x_end: {:?}, num_s: {}, num_border: {}",
+                x_start, x_end, num_s, num_border
             );
-            if let Ok(num) = num_s.parse::<u32>() {
-                let b = Booger {
-                    x_start,
-                    x_end,
-                    num,
-                    y,
-                };
-                HashSet::insert(&mut h, b);
-                println!("{:?}", h);
+            if (!bogered) {
+                if let Ok(num) = num_s.parse::<u32>() {
+                    let b = Booger {
+                        x_start,
+                        x_end,
+                        num,
+                        y,
+                    };
+                    HashSet::insert(&mut h, b);
+                    println!("{:?}", h);
+                }
             }
         }
-        acc
-    })
+    });
+    h.iter().fold(0, |acc, unique_boger| acc + unique_boger.num)
 }
