0848d20810552d6b005ab1fdbd3ce9c398d627f5
pillowtrucker <githubino@janitor.chat>
2023-12-09 15:37:14 +0100

    simply make a directed cyclical graph in rust

---
+++ src/8.rs
@@ -0,0 +1,170 @@
+#![feature(portable_simd, isqrt, iter_order_by)]
+use itertools::Itertools;
+use ndarray::{
+    parallel::prelude::{IntoParallelRefIterator, ParallelIterator},
+    ArcArray1, Array1,
+};
+use nom::{
+    branch::alt,
+    bytes::{
+        complete::{tag, take_till, take_while_m_n},
+        streaming::take_while1,
+    },
+    character::{
+        complete::{alpha0, alpha1, alphanumeric1, crlf, multispace0, multispace1, one_of},
+        is_alphabetic,
+    },
+    combinator::{cut, eof, map_res, opt},
+    error::ErrorKind,
+    multi::{many0, many1, many_m_n, many_till},
+    number::complete::be_u8,
+    sequence::{delimited, preceded, separated_pair, tuple, Tuple},
+    *,
+};
+use petgraph::graphmap::DiGraphMap;
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+    env,
+    fs::File,
+    io::{BufRead as _, BufReader, Lines},
+    num::ParseIntError,
+    ops::Index,
+    path::Path,
+    simd::StdFloat,
+};
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let fname = &args[1];
+
+    let resa: u64 = match read_lines(fname) {
+        Ok(lines) => match a(lines) {
+            Ok((_, res)) => res,
+            Err(_) => 0,
+        },
+        Err(_) => 0,
+    };
+    /*
+        let resb: u32 = match read_lines(fname) {
+            Ok(lines) => b(lines),
+            Err(_) => 0,
+        };
+    */
+    println!("part 1: {}, part 2: {}", resa, 0) //, resb);
+}
+fn read_lines<P>(filename: P) -> std::io::Result<Lines<BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(BufReader::new(file).lines())
+}
+
+fn a(lines: Lines<BufReader<File>>) -> IResult<String, u64> {
+    let the_body = lines.fold("".to_owned(), |acc: String, l| {
+        if let Ok(l) = l {
+            format!("{}{}\n", acc, l)
+        } else {
+            acc
+        }
+    });
+    print!("{}", the_body);
+    let res = peepee(&the_body);
+    println!("{:?}", res);
+    match res {
+        Ok((input, caca)) => {
+            let mut cur = poor_mans_hash("AAA");
+            let mut final_steps = 0;
+            while cur != poor_mans_hash("ZZZ") {
+                for dir in caca.caca.clone().into_iter() {
+                    final_steps += 1;
+
+                    let ok = caca.poopoo.edges(cur);
+                    let hng: Vec<_> = ok.collect();
+                    let checkdupes = hng.len();
+                    let next = if checkdupes < 2 {
+                        hng.first().unwrap()
+                    } else {
+                        hng.iter().find(|shitto| *shitto.2 == dir).unwrap()
+                    };
+                    println!(
+                        "step {final_steps:?} dir {dir:?} cur {cur:?} next {next:?} edges {hng:?}"
+                    );
+                    cur = next.1;
+                    if (cur == poor_mans_hash("ZZZ")) {
+                        break;
+                    }
+                }
+            }
+            Ok((input.to_owned(), final_steps as u64))
+        }
+        Err(e) => Err(e.to_owned()),
+    }
+}
+#[derive(Debug, Clone, Hash, Ord, PartialOrd, Eq, PartialEq)]
+enum LR {
+    L,
+    R,
+}
+use crate::LR::*;
+#[derive(Debug, Clone)]
+struct Peepee {
+    caca: Vec<LR>,
+    poopoo: DiGraphMap<Ingot, LR>,
+}
+#[derive(Debug, Clone)]
+struct RawPoopoo {
+    ingot: Ingot,
+    snakes: (Ingot, Ingot),
+}
+fn peepee(input: &str) -> IResult<&str, Peepee> {
+    let mut poopoo = DiGraphMap::<Ingot, LR>::with_capacity(512, 512);
+    let (input, caca) = many1(one_of("LR")).parse(input)?;
+    let (input, _) = tag("\n\n").parse(input)?;
+    let (input, raw_poopoos) = many1(raw_poopoo).parse(input)?;
+    for raw_poopoo in raw_poopoos {
+        poopoo.add_node(raw_poopoo.ingot);
+        poopoo.add_edge(raw_poopoo.ingot, raw_poopoo.snakes.0, L);
+        poopoo.add_edge(raw_poopoo.ingot, raw_poopoo.snakes.1, R);
+    }
+    Ok((
+        input,
+        Peepee {
+            caca: caca
+                .iter()
+                .map(|c| match c {
+                    'L' => L,
+                    'R' => R,
+                    _ => R,
+                })
+                .collect(),
+            poopoo,
+        },
+    ))
+}
+fn raw_poopoo(input: &str) -> IResult<&str, RawPoopoo> {
+    let (input, the_ingot) = ingot(input)?;
+    let (input, _) = tag(" = ").parse(input)?;
+    let (input, snakes) =
+        delimited(tag("("), separated_pair(ingot, tag(", "), ingot), tag(")")).parse(input)?;
+    let (input, _) = opt(tag("\n")).parse(input)?;
+    Ok((
+        input,
+        RawPoopoo {
+            ingot: the_ingot,
+            snakes,
+        },
+    ))
+}
+fn poor_mans_hash(input: &str) -> u64 {
+    let mut buf = [0u8; 8];
+    let len = 8.min(input.len());
+    buf[..len].copy_from_slice(&input.as_bytes()[..len]);
+    u64::from_be_bytes(buf)
+}
+type Ingot = u64;
+fn ingot(input: &str) -> IResult<&str, Ingot> {
+    let (input, ingot) = take_while_m_n(3, 3, |c: char| c.is_ascii_uppercase()).parse(input)?;
+    let fuck_you = poor_mans_hash(ingot);
+    Ok((input, fuck_you))
+}
